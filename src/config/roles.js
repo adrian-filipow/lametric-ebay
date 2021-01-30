892b5228b548cba3fe8f8031ddebdeec73b801c8
Adrian Filipow <adrianfilipow@outlook.de>
2021-01-30 02:23:06 +0000

    Add premium role and edit role rights

--- src/config/roles.js
+++ src/config/roles.js
@@ -1,8 +1,9 @@
-const roles = ['user', 'admin'];
+const roles = ['user', 'premium', 'admin'];
 
 const roleRights = new Map();
-roleRights.set(roles[0], []);
-roleRights.set(roles[1], ['getUsers', 'manageUsers']);
+roleRights.set(roles[0], ['usePrivateRoutes']);
+roleRights.set(roles[1], ['usePrivateRoutes', 'usePremiumRoutes']);
+roleRights.set(roles[2], ['getUsers', 'manageUsers']);
 
 module.exports = {
   roles,
