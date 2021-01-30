6342d76c929744b55f4633e64efd500d3354696b
Adrian Filipow <adrianfilipow@outlook.de>
2021-01-30 01:43:54 +0000

    add rate limiters

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -17,6 +17,10 @@ const redisStoreConfig = new RedisStore({
   redisURL: redis.redisURL,
 });
 
+/**
+ * auth limiter
+ * used on /v1/auth routes
+ */
 const authLimiter = rateLimit({
   store: redisStoreConfig,
   windowMs: 15 * 60 * 1000, // 15 minutes
@@ -24,6 +28,42 @@ const authLimiter = rateLimit({
   skipSuccessfulRequests: true,
 });
 
+/**
+ * default public limiter
+ * used on /v1/public routes
+ */
+const defaultPublicLimiter = rateLimit({
+  store: redisStoreConfig,
+  windowMs: 60 * 1000, // 1 minute
+  max: 20,
+  skipSuccessfulRequests: true,
+});
+
+/**
+ * default private limiter
+ * used on /v1/private routes
+ */
+const defaultPrivateLimiter = rateLimit({
+  store: redisStoreConfig,
+  windowMs: 60 * 1000, // 1 minute
+  max: 40,
+  skipSuccessfulRequests: true,
+});
+
+/**
+ * premium private limiter
+ * used on /v1/private routes when user has billing enabled
+ */
+const premiumPrivateLimiter = rateLimit({
+  store: redisStoreConfig,
+  windowMs: 60 * 1000, // 1 minute
+  max: 10000,
+  skipSuccessfulRequests: true,
+});
+
 module.exports = {
   authLimiter,
+  defaultPublicLimiter,
+  defaultPrivateLimiter,
+  premiumPrivateLimiter,
 };
