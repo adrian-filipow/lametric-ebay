c76f3cd5b10171c8d4d7703b2fea58a2cc033cfa
Adrian Filipow <adrianfilipow@outlook.de>
2021-01-30 14:12:25 +0000

    Prepare private service

--- src/services/private.service.js
+++ src/services/private.service.js
@@ -1,19 +1,29 @@
+const { ebayClient } = require('../config/ebay');
+
 /**
  * Use ebay api client
  * @param {Object} options - Query options
  * @returns {Promise<QueryResult>}
  */
-const queryPrivates = async (options) => {
-  /** MODE: KEYWORD */
+const privateService = async (options) => {
+  /**
+   * MODE: KEYWORD
+   */
   if (options.mode === 'keyword') {
-    return 'keyword';
+    const results = await ebayClient.findItemsByKeywords({
+      keywords: 'Fernseher',
+      sortOrder: 'PricePlusShippingLowest', // https://developer.ebay.com/devzone/finding/callref/extra/fndcmpltditms.rqst.srtordr.html
+    });
+    return results;
   }
-  /** MODE: AUCTION */
+  /**
+   * MODE: AUCTION
+   */
   if (options.mode === 'auction') {
     return 'auction';
   }
 };
 
 module.exports = {
-  queryPrivates,
+  privateService,
 };
