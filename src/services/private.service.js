dc8fefe3020c4fabf5e1b28ad3fce48990c0cc3a
Adrian Filipow <adrianfilipow@outlook.de>
2021-01-30 16:32:28 +0000

    Restructure private services

--- src/services/private.service.js
+++ src/services/private.service.js
@@ -1,29 +1,15 @@
-const { ebayClient } = require('../config/ebay');
+const { getBestPriceForProduct } = require('../ebay/apps/bestPriceForProduct');
 
 /**
  * Use ebay api client
  * @param {Object} options - Query options
  * @returns {Promise<QueryResult>}
  */
-const privateService = async (options) => {
-  /**
-   * MODE: KEYWORD
-   */
-  if (options.mode === 'keyword') {
-    const results = await ebayClient.findItemsByKeywords({
-      keywords: 'Fernseher',
-      sortOrder: 'PricePlusShippingLowest', // https://developer.ebay.com/devzone/finding/callref/extra/fndcmpltditms.rqst.srtordr.html
-    });
-    return results;
-  }
-  /**
-   * MODE: AUCTION
-   */
-  if (options.mode === 'auction') {
-    return 'auction';
-  }
+const getBestPriceForProductService = async (options) => {
+  const result = await getBestPriceForProduct(options);
+  return result;
 };
 
 module.exports = {
-  privateService,
+  getBestPriceForProductService,
 };
