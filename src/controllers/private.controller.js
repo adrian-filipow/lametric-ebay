e5e5a4679888dc4468f61351a074a3302f714ba0
Adrian Filipow <adrianfilipow@outlook.de>
2021-01-30 22:17:31 +0000

    Add title query

--- src/controllers/private.controller.js
+++ src/controllers/private.controller.js
@@ -3,7 +3,7 @@ const catchAsync = require('../utils/catchAsync');
 const { getBestPriceForProductService } = require('../services/private.service');
 
 const getBestPriceForProduct = catchAsync(async (req, res) => {
-  const options = pick(req.query, ['mode', 'market', 'payload', 'goal']);
+  const options = pick(req.query, ['mode', 'market', 'payload', 'goal', 'title']);
   const result = await getBestPriceForProductService(options);
   res.send(result);
 });
