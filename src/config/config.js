1a24da117e29f50de01b3fddd426563468e7df0f
Adrian Filipow <adrianfilipow@outlook.de>
2021-01-30 00:31:26 +0000

    Add redis config from env as required

--- src/config/config.js
+++ src/config/config.js
@@ -17,6 +17,7 @@ const envVarsSchema = Joi.object()
     SMTP_USERNAME: Joi.string().description('username for email server'),
     SMTP_PASSWORD: Joi.string().description('password for email server'),
     EMAIL_FROM: Joi.string().description('the from field in the emails sent by the app'),
+    REDIS_URL: Joi.string().required().description('the redis connection string'),
   })
   .unknown();
 
@@ -53,5 +54,8 @@ module.exports = {
       },
     },
     from: envVars.EMAIL_FROM,
+  },
+  redis: {
+    redisURL: envVars.REDIS_URL,
   },
 };
