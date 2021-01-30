4c7abf843d0cb6e7cd2eea52f0352825156656a0
Adrian Filipow <adrianfilipow@outlook.de>
2021-01-30 14:11:33 +0000

    Private controller

--- src/controllers/private.controller.js
+++ src/controllers/private.controller.js
@@ -1,10 +1,10 @@
 const pick = require('../utils/pick');
 const catchAsync = require('../utils/catchAsync');
-const { privateService } = require('../services');
+const { privateService } = require('../services/private.service');
 
 const getPrivates = catchAsync(async (req, res) => {
   const options = pick(req.query, ['mode', 'keywords']);
-  const result = await privateService.queryPrivates(options);
+  const result = await privateService(options);
   res.send(result);
 });
 
