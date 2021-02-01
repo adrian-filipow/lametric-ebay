92ecd0c6f51ff341bfb57784c52833a054d8daac
Adrian Filipow <adrianfilipow@outlook.de>
2021-02-01 01:36:07 +0000

    Make public route public

--- src/routes/v1/public.route.js
+++ src/routes/v1/public.route.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const auth = require('../../middlewares/auth');
 const validate = require('../../middlewares/validate');
 const privateValidation = require('../../validations/private.validation');
 const privateController = require('../../controllers/private.controller');
@@ -8,11 +7,7 @@ const router = express.Router();
 
 router
   .route('/bestPriceForProduct')
-  .get(
-    auth('usePrivateRoutes'),
-    validate(privateValidation.getBestPriceForProduct),
-    privateController.getBestPriceForProduct
-  );
+  .get(validate(privateValidation.getBestPriceForProduct), privateController.getBestPriceForProduct);
 
 module.exports = router;
 
